687466a6f3535a7cee5dd65de59d6287ca9dd608
yuuuukinkym <hurankuoukoku.nba5@gmail.com>
2024-08-11 17:51:33 +0900

    Pomodoro timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function StartButton() {
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  function Timer() {
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => setCount((c) => c + 1), 1000);
+    console.log(count);
+  }
+
+  function Pause() {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+  }
+
+  return(
+    <>
+     <button onClick={Timer}>START</button>
+     <button onClick={Pause}>PAUSE</button>
+     <p>{count}</p>
+     {isRunning ? (
+      <p>Activate</p>
+     ): (
+      <p>Pause</p>
+     )}
+    </>
   );
 }
 
-export default App;
+function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <StartButton/>
+     
+      {(typeof data.members === 'undefined') ? (
+        <p>Loadng ...</p>
+      ): (
+        data.members.map((mambers, i) => (
+          <p key={i}>{mambers}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
